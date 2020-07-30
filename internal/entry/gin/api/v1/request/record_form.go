91d811add1b828fb9085cf679b8fde36ec8c37b3
朴伟雄 <2355468258@qq.com>
2020-07-30 15:26:25 +0800

    ticket record form add oneof tag

--- internal/entry/gin/api/v1/request/record_form.go
+++ internal/entry/gin/api/v1/request/record_form.go
@@ -2,5 +2,5 @@ package request
 
 type GetRecordForm struct {
 	Period string `json:"period" form:"period" binding:"required"`
-	Type   string `json:"type" form:"type" binding:"required"`
+	Type   string `json:"type" form:"type" binding:"required,oneof=TwoToneSphere SuperLotto"`
 }
