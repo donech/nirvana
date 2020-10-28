b2c0b5abcd9fe24a0b76bcb04543ad10bb2552fd
朴伟雄 <2355468258@qq.com>
2020-10-28 10:06:47 +0800

    新增获取用户ID

--- internal/common/auth.go
+++ internal/common/auth.go
@@ -2,19 +2,19 @@ package common
 
 import (
 	"context"
-
-	"github.com/donech/tool/xlog"
+	"encoding/json"
 
 	"github.com/donech/tool/xjwt"
 )
 
 func GetUserID(ctx context.Context) int64 {
 	claims := xjwt.GetClaimsFromCtx(ctx)
-	xlog.S(ctx).Infof("claims: %#v", claims)
 	switch id := claims["id"].(type) {
 	case float64:
 		return int64(id)
-	default:
-		return 0
+	case json.Number:
+		v, _ := id.Int64()
+		return v
 	}
+	return 0
 }
